b37c16de6773a4933619b0a4c589fc3ddbe62c65
Chima <chima@youverify.co>
2023-09-25 20:03:49 +0100

    forgot-password page

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,19 @@
+import Container from "@/app/components/container";
+import Footer from "@/app/components/footer";
+import ForgotPasswordForm from "@/app/forgot-password/components/form";
+
+export default function ForgotPassword() {
+  return (
+    <>
+      <main className="">
+        <Container
+          compact
+          className="flex items-center justify-center my-16 md:my-24"
+        >
+          <ForgotPasswordForm />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
