de2ade6649f3e855cc3e62e6b6567c990c7bb1e5
Chima <chima@youverify.co>
2023-09-30 13:20:36 +0100

    sessions & auth provider

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { signOut } from "next-auth/react";
 
+import { Button } from "@/app/components/ui/button";
 import Container from "@/app/components/container";
 
 export default function Header() {
@@ -9,7 +13,7 @@ export default function Header() {
       role="banner"
       className="flex flex-shrink-0 h-14 sm:h-16 border-b border-gray-300"
     >
-      <Container className="flex items-center w-full" compact>
+      <Container className="flex items-center w-full gap-4" compact>
         <Link
           className="flex place-items-center gap-2 text-primary font-serif shrink-0 font-bold text-2xl uppercase"
           href="/"
@@ -23,6 +27,20 @@ export default function Header() {
             priority
           /> */}
         </Link>
+        <div className="flex-1 justify-end flex">
+          <Button
+            variant="outline"
+            className="font-semibold"
+            onClick={() =>
+              signOut({
+                redirect: true,
+                callbackUrl: "/login",
+              })
+            }
+          >
+            Sign Out
+          </Button>
+        </div>
       </Container>
     </header>
   );
