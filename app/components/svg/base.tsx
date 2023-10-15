1e3e7102dfb25ebed8123df50df480cb2ccaad55
Chima Ilo <collins.chima.ilo@gmail.com>
2023-10-15 23:02:01 +0100

    svg components and sidebar icons

---
+++ app/components/svg/base.tsx
@@ -0,0 +1,23 @@
+import { type ReactNode, type ComponentProps } from "react";
+import { cn } from "@/app/utils";
+
+export type SvgIconProps = ComponentProps<"svg"> & {
+  viewBox?: string;
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Svg(props: SvgIconProps): JSX.Element {
+  const { children, className, viewBox = "0 0 24 24", ...rest } = props;
+
+  return (
+    <svg
+      viewBox={viewBox}
+      aria-hidden
+      className={cn("select-none inline-block flex-shrink-0", className)}
+      {...rest}
+    >
+      {children}
+    </svg>
+  );
+}
---
+++ app/components/svg/index.ts
@@ -0,0 +1,15 @@
+import DashboardIcon from "./dashboard";
+import ReportIcon from "./reports";
+import TeamIcon from "./team";
+import SettlementIcon from "./settlement";
+import UserIcon from "./user";
+import HelpIcon from "./help";
+
+export {
+  DashboardIcon,
+  ReportIcon,
+  TeamIcon,
+  SettlementIcon,
+  UserIcon,
+  HelpIcon,
+};
